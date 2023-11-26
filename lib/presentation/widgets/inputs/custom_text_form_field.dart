9bcbcc2bddc89f9dc9d0f78dff77ab73044f122c
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-11-26 10:23:32 +0100

    Formularios tradicionales

---
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+
+  final String? label;
+  final String? hint;
+  final bool obscureText;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomTextFormField({
+    super.key,
+    this.label,
+    this.hint,
+    this.obscureText = false,
+    this.errorMessage,
+    this.onChanged,
+    this.validator
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of( context ).colorScheme;
+
+    final border = OutlineInputBorder(      
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder: border.copyWith( 
+          borderSide: BorderSide( 
+            color: colors.primary,
+            width: 1.5,            
+          ),          
+        ),
+        errorBorder: border.copyWith(
+          borderSide: BorderSide(
+            color: Colors.red.shade800
+          )
+        ),
+        focusedErrorBorder: border.copyWith(
+          borderSide: BorderSide(
+            color: Colors.red.shade800
+          )
+        ),
+        isDense: true,
+        label: label != null ? Text( label! ): null,
+        hintText: hint,        
+        errorText: errorMessage,
+        focusColor: colors.primary,        
+      ),
+    );
+  }
+}
